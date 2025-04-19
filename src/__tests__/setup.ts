f893ce3545798d0965f41e3b782178c3b5c2c981
AminMasharqa <126580497+AminMasharqa@users.noreply.github.com>
2025-04-19 20:19:00 +0300

    modefied the time out

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -5,7 +5,7 @@
 // This file is used for global Jest setup and is not a test file itself
 
 // Increase timeout for all tests
-jest.setTimeout(30000);
+jest.setTimeout(300000);
 
 // Global setup for tests
 beforeAll(() => {
